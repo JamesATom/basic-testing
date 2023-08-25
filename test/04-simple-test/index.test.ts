bbbce401e35bb2338ac50e0c784d721328d83b2e
Abdulaziz <abduhakimovabdulaziz7@gmail.com>
2023-08-25 06:15:33 +0500

    feat: done with 04-simple-test folder

--- test/04-simple-test/index.test.ts
+++ test/04-simple-test/index.test.ts
@@ -1,44 +1,103 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import { getBankAccount } from '../../src/04-simple-test/index';
+import lodash from 'lodash';
 
 describe('BankAccount', () => {
     test('should create account with initial balance', () => {
-      // Write your test here
+        // arrange
+        const expected = 3;
+        // act
+        const actual = getBankAccount(3);
+        // assert
+        expect(actual.getBalance()).toBe(expected);
     });
   
     test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-      // Write your test here
+        // arrange
+        const expected = new Error('Insufficient funds: cannot withdraw more than 3');
+        // act
+        const actual = getBankAccount(3);
+        // assert
+        expect(() => actual.withdraw(4)).toThrow(expected.message);
     });
   
     test('should throw error when transferring more than balance', () => {
-      // Write your test here
+        // arrange
+        const exptected1 = new Error('Insufficient funds: cannot withdraw more than 3');
+        const exptected2 = new Error('Transfer failed');
+        const toAccount = getBankAccount(5);
+        // act
+        const actual = getBankAccount(3);
+        // assert
+        expect(() => actual.transfer(3, actual)).toThrow(exptected2.message);
+        expect(() => actual.transfer(5, toAccount)).toThrow(exptected1.message);
     });
   
     test('should throw error when transferring to the same account', () => {
-      // Write your test here
+        // arrange
+        const expected = new Error('Transfer failed');
+        // act
+        const actual = getBankAccount(3);
+        // assert
+        expect(() => actual.transfer(3, actual)).toThrow(expected.message); 
     });
   
     test('should deposit money', () => {
-      // Write your test here
+        // arrange
+        const expected = 5;
+        // act
+        const actual = getBankAccount(3);
+        // assert
+        expect(actual.deposit(2).getBalance()).toBe(expected);
     });
   
     test('should withdraw money', () => {
-      // Write your test here
+        // arrange
+        const expected = 3;
+        // act
+        const actual = getBankAccount(5);
+        // assert
+        expect(actual.withdraw(2).getBalance()).toBe(expected);
     });
   
     test('should transfer money', () => {
-      // Write your test here
+        // arrange
+        const expected = 3;
+        const toAccount = getBankAccount(1);
+        // act
+        const actual = getBankAccount(5);
+        // assert
+        expect(actual.transfer(2, toAccount).getBalance()).toBe(expected);
     });
   
     test('fetchBalance should return number in case if request did not failed', async () => {
-      // Write your tests here
+        // arrange
+        jest.spyOn(lodash, 'random').mockReturnValue(1);
+        // act
+        const actual = getBankAccount(3);
+        const balance = await actual.fetchBalance();
+        // assert
+        expect(typeof balance).not.toBeNaN();
     });
   
     test('should set new balance if fetchBalance returned number', async () => {
-      // Write your tests here
+        // arrange
+        const actual = getBankAccount(3);
+        jest.spyOn(actual, 'fetchBalance').mockImplementation(() => Promise.resolve(3));
+        // act
+        await actual.synchronizeBalance();
+        // assert
+        expect(actual.getBalance()).toBe(3);``
     });
   
     test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
-      // Write your tests here
+        // arrange
+        const error = new Error('Synchronization failed');
+        const expected = getBankAccount(3);
+        jest.spyOn(expected, 'fetchBalance').mockImplementation(() => Promise.resolve(null));
+        // act
+        const actual = expected.synchronizeBalance();
+        // assert
+        expect(actual).rejects.toThrow(error.message);
     });
   });
